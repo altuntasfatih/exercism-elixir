0f4f282fcdd2bdd438d96b7a3cd58afd40fb439b
fatih.altuntas <fatih.altuntas@trendyol.com>
2020-05-31 17:20:06 +0300

    solution was refactored

--- prime-factors/lib/prime_factors.ex
+++ prime-factors/lib/prime_factors.ex
@@ -1,40 +1,48 @@
 defmodule PrimeFactors do
   @doc """
   Compute the prime factors for 'number'.
-
   The prime factors are prime numbers that when multiplied give the desired
   number.
-
   The prime factors of 'number' will be ordered lowest to highest.
+
+  Test results ->
+    PrimeFactorsTest
+      * test 4 (0.00ms)
+      * test 93819012551 (0.3ms)
+      * test 1 (0.00ms)
+      * test 2 (0.00ms)
+      * test 8 (0.00ms)
+      * test 10000000055 (0.6ms)
+      * test 901255 (0.00ms)
+      * test 625 (0.00ms)
+      * test 3 (0.00ms)
+      * test 9 (0.00ms)
+      * test 6 (0.00ms)
+      * test 27 (0.00ms)
+
+    Finished in 0.03 seconds
+    12 tests, 0 failures
   """
   @spec factors_for(pos_integer) :: [pos_integer]
   def factors_for(number) do
-    find_factors(number, 2, [])
+    factors_for(number, 2, [])
     |> Enum.reverse()
   end
 
-  def find_factors(1, _, acc), do: acc
+  def factors_for(1, _, acc), do: acc
 
-  def find_factors(number, factor, acc) when rem(number, factor) == 0 do
-    if is_prime?(factor) do
-      div(number, factor)
-      |> check_itsel(factor, [factor | acc])
-    else
-      find_factors(number, factor + 1, acc)
-    end
-  end
+  def factors_for(number, factor, acc) when rem(number, factor) == 0 do
+    number = div(number, factor)
+    acc = [factor | acc]
 
-  def find_factors(number, factor, acc) do
-    find_factors(number, factor + 1, acc)
-  end
-
-  def check_itsel(number, factor, acc) do
     if is_prime?(number) do
       [number | acc]
     else
-      find_factors(number, factor, acc)
+      factors_for(number, factor, acc)
     end
   end
+
+  def factors_for(number, factor, acc), do: factors_for(number, factor + 1, acc)
 
   def is_prime?(2), do: true
   def is_prime?(3), do: true
@@ -51,5 +59,6 @@ defmodule PrimeFactors do
   def is_prime?(number, factor, limit) when factor < limit do
     is_prime?(number, factor + 1, limit)
   end
+
   def is_prime?(_, _, _), do: true
 end
