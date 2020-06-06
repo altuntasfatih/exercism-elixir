28e414261e96af79accc7b453c447362b15c48b1
fatih.altuntas <fatih.altuntas@trendyol.com>
2020-06-06 18:10:38 +0300

    refacto solution of perfect numbers

--- perfect-numbers/lib/perfect_numbers.ex
+++ perfect-numbers/lib/perfect_numbers.ex
@@ -9,22 +9,21 @@ defmodule PerfectNumbers do
   :abundant if the aliquot sum is greater than `number`
   :deficient if the aliquot sum is less than `number`
   """
-  def classify(1), do: {:ok, :deficient}
 
   def classify(number) when number < 1,
     do: {:error, "Classification is only possible for natural numbers."}
 
   @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
   def classify(number) do
-    limit = div(number, 2)
+    sum = aliquot_sum(div(number, 2), 0, number)
 
-    aliquot_sum(limit, 0, number)
-    |> class(number)
+    cond do
+      sum == 1 and number == 1 -> {:ok, :deficient}
+      sum == number -> {:ok, :perfect}
+      sum < number -> {:ok, :deficient}
+      true -> {:ok, :abundant}
+    end
   end
-
-  defp class(sum, sum), do: {:ok, :perfect}
-  defp class(sum, number) when sum > number, do: {:ok, :abundant}
-  defp class(_, _), do: {:ok, :deficient}
 
   defp aliquot_sum(0, acc, _), do: acc
 
