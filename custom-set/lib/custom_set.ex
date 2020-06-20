a41a2b150c1bc299ceaddfde5af795aeb84c5854
fatih.altuntas <fatih.altuntas@trendyol.com>
2020-06-20 10:08:26 +0300

    solution of custom-set was refactored

--- custom-set/lib/custom_set.ex
+++ custom-set/lib/custom_set.ex
@@ -1,10 +1,8 @@
 defmodule CustomSet do
-  defstruct map: nil
+  defstruct map: %{}
   @opaque t :: %__MODULE__{map: map}
 
   @spec new(Enum.t()) :: t
-  def new([]), do: empty_set()
-
   def new(enumerable) do
     set =
       Enum.reduce(enumerable, %{}, fn i, acc ->
@@ -14,31 +12,20 @@ defmodule CustomSet do
     %CustomSet{map: set}
   end
 
-  defp empty_set(), do: %CustomSet{}
-
   @spec empty?(t) :: boolean
-  def empty?(%CustomSet{map: nil}), do: true
-  def empty?(_), do: false
+  def empty?(set), do: map_size(set.map) == 0
 
-  @spec contains?(t, any) :: boolean
-  def contains?(%CustomSet{map: nil}, _), do: false
   def contains?(custom_set, element) do
     Map.has_key?(custom_set.map, element)
   end
 
   @spec subset?(t, t) :: boolean
-  def subset?(%CustomSet{map: set}, %CustomSet{map: set}), do: true
-  def subset?(%CustomSet{map: nil}, _), do: true
-  def subset?(_, %CustomSet{map: nil}), do: false
   def subset?(%CustomSet{map: set_1}, %CustomSet{map: set_2}) do
     Map.keys(set_1)
     |> Enum.all?(fn item -> Map.has_key?(set_2, item) end)
   end
 
   @spec disjoint?(t, t) :: boolean
-  def disjoint?(%CustomSet{map: nil}, %CustomSet{map: nil}), do: true
-  def disjoint?(%CustomSet{map: nil}, _), do: true
-  def disjoint?(_, %CustomSet{map: nil}), do: true
   def disjoint?(%CustomSet{map: set_1}, %CustomSet{map: set_2}) do
     Map.keys(set_1)
     |> Enum.all?(fn item -> not Map.has_key?(set_2, item) end)
@@ -49,15 +36,11 @@ defmodule CustomSet do
   def equal?(_, _), do: false
 
   @spec add(t, any) :: t
-  def add(%CustomSet{map: nil}, element), do: new([element])
   def add(%CustomSet{map: set}, element) do
     %CustomSet{map: Map.put(set, element, nil)}
   end
 
   @spec intersection(t, t) :: t
-  def intersection(set, set), do: set
-  def intersection(%CustomSet{map: nil}, _), do: empty_set()
-  def intersection(_, %CustomSet{map: nil}), do: empty_set()
   def intersection(set_1, set_2) do
     Map.keys(set_1.map)
     |> Enum.filter(fn item -> Map.has_key?(set_2.map, item) end)
@@ -65,10 +48,6 @@ defmodule CustomSet do
   end
 
   @spec difference(t, t) :: t
-  def difference(%CustomSet{map: nil}, %CustomSet{map: nil}), do: empty_set()
-  def difference(custom_set, %CustomSet{map: nil}), do: custom_set
-  def difference(%CustomSet{map: nil}, _), do: empty_set()
-
   def difference(set_1, set_2) do
     Map.keys(set_1.map)
     |> Enum.filter(fn item -> not Map.has_key?(set_2.map, item) end)
@@ -76,8 +55,6 @@ defmodule CustomSet do
   end
 
   @spec union(t, t) :: t
-  def union(custom_set, %CustomSet{map: nil}), do: custom_set
-  def union(%CustomSet{map: nil}, custom_set), do: custom_set
   def union(custom_set_1, custom_set_2) do
     %CustomSet{map: Map.merge(custom_set_1.map, custom_set_2.map)}
   end
