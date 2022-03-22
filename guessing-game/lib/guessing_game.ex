82e7eb9e781691aef010dd1ddf3bc683ccd8b4bd
Fatih Altuntaş <altuntasfatih42@gmail.com>
2022-03-22 15:44:05 +0300

    Refactor guessing_game

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,10 +1,9 @@
 defmodule GuessingGame do
-  def compare(_), do: "Make a guess"
+  def compare(_, guess \\ :no_guess)
   def compare(_, :no_guess), do: "Make a guess"
-
   def compare(secret_number, secret_number), do: "Correct"
 
-  def compare(secret_number, guess) when secret_number == guess - 1 or secret_number == guess + 1,
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1,
     do: "So close"
 
   def compare(secret_number, guess) when guess > secret_number, do: "Too high"
