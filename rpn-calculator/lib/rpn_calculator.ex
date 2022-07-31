30e2cae6d96b2b82d19d4d19cd21aaf7dcb6df43
Fatih Altuntas <altuntasfatih42@gmail.com>
2022-07-31 14:26:31 +0300

    Add solution of need-for-speed

---
+++ rpn-calculator/lib/rpn_calculator.ex
@@ -0,0 +1,21 @@
+defmodule RPNCalculator do
+  def calculate!(stack, operation) do
+    operation.(stack)
+  end
+
+  def calculate(stack, operation) do
+    try do
+      {:ok, calculate!(stack, operation)}
+    rescue
+      _ -> :error
+    end
+  end
+
+  def calculate_verbose(stack, operation) do
+    try do
+      {:ok, calculate!(stack, operation)}
+    rescue
+      e in ArgumentError -> {:error, e.message}
+    end
+  end
+end
